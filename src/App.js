6f8ca4735ae2b2f83306422590628babf572e9f4
Lanzhen Wang <lanzhenwang@Lanzhens-MacBook-Pro.local>
2019-06-16 15:43:11 -0500

    added gh-pages and deployment scripts

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>I am creating a tic-tac-toe game.</p>
+        <p>Welcome to the tic-tac-toe game!</p>
         <Tictactoe />
       </header>
     </div>
