99e138f315229f7949b9eab702a160adccdd3a5b
Lanzhen Wang <lanzhenwang@Lanzhens-MacBook-Pro.local>
2019-06-16 15:36:32 -0500

    Updates

---
+++ precache-manifest.046f3073680333cc5533433cf0d9b9cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924db13d7b0f2cb64dbfab87d90b9b95",
+    "url": "/react-tictactoe-practice/index.html"
+  },
+  {
+    "revision": "78fcd3bad5c1bd16d2f5",
+    "url": "/react-tictactoe-practice/static/css/main.84edd48d.chunk.css"
+  },
+  {
+    "revision": "6652b58fd2b81d2f0866",
+    "url": "/react-tictactoe-practice/static/js/2.78d60315.chunk.js"
+  },
+  {
+    "revision": "78fcd3bad5c1bd16d2f5",
+    "url": "/react-tictactoe-practice/static/js/main.7b685e30.chunk.js"
+  },
+  {
+    "revision": "0607301e7220537b6fb7",
+    "url": "/react-tictactoe-practice/static/js/runtime~main.8ddebe1c.js"
+  }
+]);
